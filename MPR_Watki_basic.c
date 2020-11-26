07aeb5e1b825e263d38ed6affc72aadfaaeede5f
MaverickTheDude <38852811+MaverickTheDude@users.noreply.github.com>
2020-11-26 19:50:17 +0100

    Update and rename MPR_Watki_basic to MPR_Watki_basic.c

--- MPR_Watki_basic.c
+++ MPR_Watki_basic.c
@@ -5,43 +5,42 @@
 # include <stdint.h> // Zeby pozbyc sie warninga o roznych rozmiarach rzutowania
 /* (intptr_t) zastepuje typ (void*) */
 
-# define STEP 2
+# define STEP 1
 # define NUM_THREADS 8
 
 typedef struct {
 	int cnt; // numer watku
 	int sum; // suma kontrolna
 	const char * msg; // wiadomosc
+
 } thread_data;
 
 /* Funkcja wykonywana przez watek */
 void* start_routine( void* arg) {
 	int i, id;
-	//id = (int)(intptr_t)arg;			// Przekazanie 0 w argumencie - wyswietli 1
-	//id = pthread_self();  // Wyswietli 2 dla pierwszego (wszystkich?) watku
 
 	//Zad 1
-	thread_data* dane_pointer = arg;
-	id = (*dane_pointer).cnt; // dane_pointer->cnt;
+	thread_data* dane_pointer = (thread_data*)arg;
+	id = dane_pointer->cnt;
 
 	/* Wyswietl nr przekazanego watku i krok */
 	for(i = 0; i < STEP ; i++) {
 		printf("\tWatek : %d, step = %d\n", id , i);
-		printf("message: %s\n", dane_pointer->msg);
-		sleep(1);
+		printf("\tmessage: %s\n", dane_pointer->msg);
+		//sleep(1);
 	}
-	((thread_data*)arg)->sum = 26;  // Added: zapis przez referencję do struktury która jest argumentem
-	pthread_exit( (void*)(intptr_t)id );
+	dane_pointer->sum = id;
+	//pthread_exit( (void*)(intptr_t)id );
 	//pthread_exit(NULL);
 	return NULL;
 }
 
 /* The main program . */
-int main() {
+void* initialize(void* arg) {
 	int i, rc;
 	pthread_t thread_id[NUM_THREADS];
 	void* status;
-	pthread_attr_t attr; // Nie wskaznik?
+	pthread_attr_t attr;
 
 	// Zad 1:
 	thread_data dane[NUM_THREADS];
@@ -67,13 +66,16 @@ int main() {
 	pthread_attr_init( &attr );
 	/* Ustawienie atrybut dolaczalnosci */
 	pthread_attr_setdetachstate(&attr , PTHREAD_CREATE_JOINABLE );
+	int policy;
+	pthread_attr_getschedpolicy(&attr, &policy);
+	int* ptr = (int*) arg;
+	*ptr = policy;
 
 	for(i = 0; i < NUM_THREADS ; i++) {
 		/* Utworz nowy watek , ktory bedzie wykonywal funkcje start routine . */
 		/* Przekaz argument i do watku */
 		printf("Tworze watek nr: %d\n", i);
-		rc = pthread_create(& thread_id[i], &attr , &start_routine , &(dane[i]) );
-		//rc = pthread_create(& thread_id[i], &attr , &start_routine , (void*)(intptr_t)i);
+		rc = pthread_create(& thread_id[i], &attr , &start_routine , (void*)(dane+i) );
 		/* W przypadku , gdy nie uda sie utworzyc watku */
 		if (rc) {
 			printf(" Blad. Kod powrotu z funkcji pthread create(): %d\n", rc);
@@ -92,9 +94,26 @@ int main() {
 		printf (" Watek glowny dolaczyl watek %d o statusie %d\n", i, (int)(intptr_t)status );
 	}
 	//printf("sizeof int: %d, sizeof void*: %d\n",sizeof(int),sizeof(void*)); -> 4 vs 8 stad jest warning
-	printf(" Watek glowny zakonczony. Wyjscie \n");
-	// ADDED (loop):
+
 	for (i = 0; i < NUM_THREADS; i++)
-		printf("%d \n",dane[i].sum);
+		printf("suma kontrolna %d: %d \n", i, dane[i].sum);
+
+	printf(" Watek glowny zakonczony. Wyjscie \n");
 	pthread_exit( NULL );
+}
+
+int main() {
+	pthread_t thread_id;
+	void* status;
+	int policy_arg;
+	int rc = pthread_create(&thread_id, NULL , &initialize , (void*) &policy_arg);
+	if (rc) {
+		printf (" Blad . Kod powrotu z funkcji pthread join (): %d\n",rc);
+		exit(1);
+	}
+
+	rc = pthread_join(thread_id, &status);
+	printf("strategia szeregowania watku master(): %d \n", policy_arg);
+	printf ("Watek main() dolaczyl watek master(). Status: %d\n", (int)(intptr_t)status );
+	return 0;
 }
